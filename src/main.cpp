c5129e06044022d6388d202067389744390bc2f5
Adamczyk Piotr <admin@adamczykpiotr.com>
2022-12-01 18:08:03 +0100

    Project: Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,61 @@
+#include "Platform/PlatformPinout.hpp"
+
+#include "Hardware/Analog/Analog.hpp"
+#include "Hardware/Relay/Relay.hpp"
+#include "Hardware/DAC/MCP4922.hpp"
+
+//#include "Core/Orchestrator.hpp"
+
 int main() {
+    /**
+     * Initialize hardware
+     */
+
+    // CAN
+    // TODO: Implement interface
+
+
+    // Analog
+    Analog adc;
+    adc.init();
+
+    // DAC
+    MCP4922 dac(spi0, PlatformPinout::DAC_CS_PIN, PlatformPinout::DAC_TX_PIN, PlatformPinout::DAC_SCK_PIN, PlatformPinout::DAC_LDAC_PIN);
+    dac.init();
+
+    // Relay
+    Relay relay(PlatformPinout::GPIO_RELAY_PIN);
+    relay.init();
+
+    // LED
+    Relay led(PlatformPinout::LED_PIN);
+    led.init();
+
+    /**
+     * Global variables
+     */
+
+    // Controller State
+
+    // Error handler
+
+    /**
+     * Orchestrator
+     */
+
+    // Initialize orchestrator
+//    Orchestrator orchestrator(
+////            dac,
+////            relay,
+////            led,
+//    );
+//    orchestrator.init();
+//
+//    // Run auxiliary orchestrator thread (CAN + USB communication)
+//    orchestrator.handleCommunication();
+//
+//    // Run main orchestrator thread (calculations, safety, outputs)
+//    orchestrator.handleMainTasks();
+
     return 0;
 }
