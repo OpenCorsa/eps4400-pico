4a754badf5996dfae741079e4e618e4372af1a40
Adamczyk Piotr <admin@adamczykpiotr.com>
2022-12-09 21:21:39 +0100

    Monitors: Prepare classes for future implementations

---
+++ src/Monitoring/Analog/DriverTorqueMonitor.hpp
@@ -0,0 +1,7 @@
+#pramga once
+
+class DriverTorqueMonitor {
+
+    // Some kind of clever maths to determine whether the driver is steering or the system itself
+
+};
---
+++ src/Monitoring/PlatformWatchdog.hpp
@@ -0,0 +1,5 @@
+#pramga once
+
+class PlatformWatchdog {
+
+};
